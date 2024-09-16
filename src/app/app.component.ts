c4cf4cc74ee97098579972162bf86a9edfbbe170
Mahadali-miryam <mahadali-miryam@outlook.com>
2024-09-16 18:46:03 +0200

    fin de la page contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from "./contact/contact.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import emailjs from 'emailjs-com';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../../environments/environment'; 
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  contact = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  constructor() {}
+
+  onSubmit() {
+    let templateParams = {
+      from_name: this.contact.name,
+      from_email: this.contact.email,
+      message: this.contact.message
+    };
+    // Envoi du formulaire avec EmailJS en utilisant les identifiants stockés dans l'objet environment
+    emailjs.send(environment.emailjsServiceId, environment.emailjsTemplateId, templateParams, environment.emailjsUserId)
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+      alert('Votre message a été envoyé avec succès !');
+    }, (err) => {
+      console.log('FAILED...', err);
+      alert('Erreur lors de l\'envoi du message. Veuillez réessayer.');
+    });
+  }
+}
+
+
