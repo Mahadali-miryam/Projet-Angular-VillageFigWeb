711a6a7c007efd60859953c131672a54756cdf81
Mahadali-miryam <mahadali-miryam@outlook.com>
2024-09-18 18:35:48 +0200

    modif

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [  
-    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
-  ];
-
+    
+  ]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   standalone: true,
   imports: [],
+
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
